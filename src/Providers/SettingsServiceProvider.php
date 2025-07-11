4dc8823b0709cf4e7a390019935e5b2d65cea3ba
Maksym Sliusarchuk <max.slyusarchuk@gmail.com>
2025-07-11 12:06:50 +0300

    Add caching for settings, update ServiceProvider, and enhance package configuration

--- src/Providers/SettingsServiceProvider.php
+++ src/Providers/SettingsServiceProvider.php
@@ -2,17 +2,26 @@
 
 namespace DamichiXL\Settings\Providers;
 
+use DamichiXL\Settings\Models\Settings;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class SettingsServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-
-    }
+    public function register(): void {}
 
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../migrations');
+
+        if (Schema::connection('sqlite')->hasTable('settings')) {
+            $settings = Cache::rememberForever(
+                'settings',
+                fn () => Settings::all()->pluck('value', 'key')->toArray()
+            );
+
+            config($settings);
+        }
     }
 }
